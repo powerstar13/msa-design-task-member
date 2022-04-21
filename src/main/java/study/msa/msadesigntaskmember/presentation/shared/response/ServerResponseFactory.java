dc3802222036c3487b1a064f9d206c7c6807fa17
홍준성 <powerstar13@hanmail.net>
2022-04-22 04:50:36 +0900

    ServerResponse  > 재사용 위해 모듈화

---
+++ src/main/java/study/msa/msadesigntaskmember/presentation/shared/response/ServerResponseFactory.java
@@ -0,0 +1,10 @@
+package study.msa.msadesigntaskmember.presentation.shared.response;
+
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public interface ServerResponseFactory {
+
+    Mono<ServerResponse> successOnly();
+    Mono<ServerResponse> successBodyValue(Object response);
+}
