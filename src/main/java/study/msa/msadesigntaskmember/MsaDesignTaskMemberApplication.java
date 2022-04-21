d4d80273566c123fcc623bc36c66649e7df0cb3b
홍준성 <powerstar13@hanmail.net>
2022-04-22 04:49:45 +0900

    git init > MSA 설계 과제 > README 작성 및 YAML에 Eureka Client  역할 설정

---
+++ src/main/java/study/msa/msadesigntaskmember/MsaDesignTaskMemberApplication.java
@@ -0,0 +1,15 @@
+package study.msa.msadesigntaskmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient // Eureka Client 역할을 수행한다. YAML에 명시한 유레카 서버에 해당 애플리케이션을 등록한다.
+public class MsaDesignTaskMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaDesignTaskMemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/msa/msadesigntaskmember/MsaDesignTaskMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package study.msa.msadesigntaskmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaDesignTaskMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
